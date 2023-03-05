ee6b925051b55f97bf9647dec2809d1292a4829d
dimitar <90458809+DimOps@users.noreply.github.com>
2023-03-05 12:30:13 +0200

    Added Status model. Create_db.py updated with recommended declarative style.

--- backend/create_db.py
+++ backend/create_db.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
-
+from sqlalchemy import create_engine, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from typing import List
 
 engine = create_engine("sqlite:///healthchecks.db", echo=True)
 Base = declarative_base()
@@ -9,17 +9,25 @@ Base = declarative_base()
 class Check(Base):
     __tablename__ = "checks"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(300))
-    host = Column(String(300))
-    type = Column(String(300))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column()
+    host: Mapped[str] = mapped_column()
+    type: Mapped[str] = mapped_column()
 
-    def __repr__(self):
-        return "<Check(name'%s', host='%s', type='%s')>" % (
-            self.name,
-            self.host,
-            self.type,
-        )
+    current_state: Mapped[List["Status"]] = relationship(back_populates="checking")
+
+
+class Status(Base):
+    __tablename__ = "status"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    ping_id: Mapped[int] = mapped_column()
+    status: Mapped[str] = mapped_column()
+    last_down_start: Mapped[int] = mapped_column()
+    last_down_end: Mapped[int] = mapped_column()
+    check_id = mapped_column(ForeignKey("checks.id"))
+
+    checking: Mapped["Check"] = relationship(back_populates="current_state")
 
 
 Base.metadata.create_all(engine)
