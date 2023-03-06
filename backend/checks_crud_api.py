1d1fb7358325fcf06409aa0b055e67f85acae9b6
dimitar <90458809+DimOps@users.noreply.github.com>
2023-03-06 11:46:55 +0200

    Add an outage summary functionality.

--- backend/checks_crud_api.py
+++ backend/checks_crud_api.py
@@ -12,3 +12,8 @@ class ChecksCrudApi:
     def get_checks_list(self):
         r = requests.get('https://api.pingdom.com/api/3.1/checks', headers=self.auth)
         return r.json()
+
+    def get_check_details(self, check_id, *args):
+        # report defaults on one week according to documentation
+        r = requests.get(f'https://api.pingdom.com/api/3.1/summary.outage/{check_id}', headers=self.auth)
+        return r.json()
