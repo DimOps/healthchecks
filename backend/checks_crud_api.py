559ce8967eae1f653571b3a0987caf47715ab5b6
dimitar <90458809+DimOps@users.noreply.github.com>
2023-03-07 09:33:57 +0200

    Added timeframe option in outage report method.

--- backend/checks_crud_api.py
+++ backend/checks_crud_api.py
@@ -6,24 +6,28 @@ from dotenv import load_dotenv
 class ChecksCrudApi:
     def __init__(self):
         load_dotenv()
+        self.url = 'https://api.pingdom.com/api/3.1'
         self.key = os.getenv('TOKEN')
         self.auth = {'Authorization': f'Bearer {self.key}'}
 
     def get_checks_list(self):
-        r = requests.get('https://api.pingdom.com/api/3.1/checks', headers=self.auth)
+        r = requests.get(f'{self.url}/checks', headers=self.auth)
         return r.json()
 
-    def get_check_outage_summary(self, check_id, *args):
-        # report defaults on one week according to documentation
-        r = requests.get(f'https://api.pingdom.com/api/3.1/summary.outage/{check_id}', headers=self.auth)
+    def get_check_outage_summary(self, check_id, **kwargs):
+        q_params = []
+        # report defaults on one week according to Pingdom API documentation
+        if kwargs:
+            q_params = [(k, v) for k, v in kwargs.items()]
+        r = requests.get(f'{self.url}/summary.outage/{check_id}', headers=self.auth, params=q_params)
         return r.json()
 
     def create_check(self, data):
-        r = requests.post(f'https://api.pingdom.com/api/3.1/checks', headers=self.auth, json=data)
+        r = requests.post(f'{self.url}/checks', headers=self.auth, json=data)
         return r.json()
 
     def delete_check(self, check_id):
-        r = requests.delete(f'https://api.pingdom.com/api/3.1/checks/{check_id}', headers=self.auth)
+        r = requests.delete(f'{self.url}/checks/{check_id}', headers=self.auth)
         return r.json()
 
     def delete_many_checks(self, list_ids):
@@ -33,5 +37,5 @@ class ChecksCrudApi:
             "delcheckids": f"{string_list}"
         }
 
-        r = requests.delete('https://api.pingdom.com/api/3.1/checks', headers=self.auth, json=data)
+        r = requests.delete(f'{self.url}/checks', headers=self.auth, json=data)
         return r.json()
