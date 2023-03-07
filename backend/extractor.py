93b08add0830fa4a6047548e7e9a1edfec1efbf2
dimitar <90458809+DimOps@users.noreply.github.com>
2023-03-07 14:23:34 +0200

    Updated the extractor.py to accept class name parameter and call class, based on it.

--- backend/extractor.py
+++ backend/extractor.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from create_db_models import Status
+from create_db_models import Status, Check
 
 
-def db_extractor():
+def db_extractor(classname):
     engine = create_engine("sqlite:///healthchecks.db", echo=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    data = session.query(Status).all()
-    session.close()
-
-    return data
+    if classname == 'Check':
+        data = session.query(Check).all()
+        session.close()
+        return data
+    elif classname == 'Status':
+        data = session.query(Status).all()
+        session.close()
+        return data
