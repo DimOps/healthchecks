b19287c3f40a4b3d08433232744d9e9925a7a472
dimitar <90458809+DimOps@users.noreply.github.com>
2023-03-09 17:51:43 +0200

    Created an updater script to update state of DB records.

---
+++ backend/update_state.py
@@ -0,0 +1,46 @@
+from create_db_models import Status, Check
+from checks_crud_api import ChecksCrudApi
+from sqlalchemy import create_engine, update, select
+from sqlalchemy.orm import sessionmaker
+import time
+
+engine = create_engine("sqlite:///healthchecks.db", echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def change_record(check, ping):
+    if check.ping_id != ping['id']:
+        raise Exception('Status table IDs does NOT match Pingdom IDs! Sync data!')
+    else:
+        ping_dict_stats = dict((key, value) for (key, value) in ping.items()
+                               if key == 'status'
+                               or key == 'lastdownstart'
+                               or key == 'lastdownend')
+
+        db_status_record = {'status': check.status,
+                            'lastdownstart': check.lastdownstart,
+                            'lastdownend': check.lastdownend}
+
+        for k, v in ping_dict_stats.items():
+            if ping_dict_stats[k] != db_status_record[k]:
+                u = {f'{k}': ping_dict_stats[k]}
+                print(u)
+                session.query(Status).filter(Status.ping_id == ping['id']).update(u)
+        session.commit()
+
+
+def update_state():
+    checks_list = session.query(Status).all()
+    pings_list = ChecksCrudApi().get_checks_list()
+
+    [change_record(c, p) for c, p in zip(checks_list, pings_list['checks'])
+     if c.status != p['status']
+     or c.ping_id != p['id']]
+
+
+start = time.time()
+update_state()
+session.close()
+end = time.time()
+print(end - start)
