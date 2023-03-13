a81131c0a0e75c9a00c2d162ce3bb3272fe91662
dimitar <90458809+DimOps@users.noreply.github.com>
2023-03-13 17:42:16 +0200

    Identical host - type pairs notification added.

--- backend/update_db.py
+++ backend/update_db.py
@@ -14,19 +14,26 @@ Session = sessionmaker(bind=engine)
 session = Session()
 db_data = session.query(Check).all()
 
-# check if someone changes the name but not the host and type and just state it
-
 stringified_checks = [f"{hc['name']} - {hc['host']} - {hc['type']}" for hc in json_data]
 residuals = [item for item, count in Counter(stringified_checks).items() if count > 1]
+
 if len(residuals) > 0:
     res = '\n'.join(residuals)
-    raise Exception(f'Checks with the following "name - host - type" configuration are duplicated: {res}')
+    raise Exception(f'Checks with the following "name - host - type" configuration are duplicated:\n{res}')
 
 json_checks_name = [c['name'] for c in json_data]
 eq_names = [item for item, count in Counter(json_checks_name).items() if count > 1]
+
 if len(eq_names) > 0:
     names_str = ', '.join([n for n in eq_names])
     raise Exception(f'Check/s have the following names duplicated: {names_str}')
+
+host_type_pairs = [f"{hc['host']} - {hc['type']}" for hc in json_data]
+residuals = [item for item, count in Counter(stringified_checks).items() if count > 1]
+
+if len(residuals) > 0:
+    res = '\n'.join(residuals)
+    print(f'The following host - type pairs have been duplicated with unique names:\n{res}')
 
 deletions = []
 for db_instance in db_data:
