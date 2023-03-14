957ebe636c1dc8a3fc262e04122c457e27585197
dimitar <90458809+DimOps@users.noreply.github.com>
2023-03-14 09:22:19 +0200

    Update of outage report on submit functionality added.

--- frontend/src/components/CheckComponent/CheckComponent.js
+++ frontend/src/components/CheckComponent/CheckComponent.js
@@ -1,23 +1,39 @@
 import { useState } from "react";
 import styles from "./CheckComponent.module.css"
 
-import { FetchReport } from "../../utils";
 import moment from "moment";
 
 
 const CheckComponent = (props) => {
     const [hours, setHours] = useState(0)
+    const [outage, setOutage] = useState({'unknown': 100, 'up': 0, 'down': 0})
+    const kwargs = { 'timefrom': 0, 'timeto': 0 }
 
-    let outagePercentage = {'unknown': 100, 'up': null, 'down': null}
+    const FetchReport = (pingId, kwargs) => {       
+        const body = {
+            'checkId': pingId,
+            'kwargs': kwargs
+        }
+        const url = `http://localhost:8000/api/summary`
 
-    const submitHandler = async (e) => {
+        fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json",
+            },
+            body: JSON.stringify(body),
+        })
+            .then(res => res.json())
+            .then(result => setOutage(result))   
+    };
+
+    const submitHandler = (e) => {
         e.preventDefault();
-        const kwargs = {'from': 0, 'to': 0}
         const hoursBack = hours
-        kwargs.to = Date.now()
-        kwargs.from = Date.now() - Number(hoursBack)*60000
-        const res = await FetchReport(kwargs)
-        console.log(res)
+        kwargs.timeto = Math.floor(Date.now() / 1000)
+        kwargs.timefrom = Math.floor((Date.now() - Number(hoursBack) * 60000) / 1000)
+        FetchReport(props.pingId, kwargs)
+        console.log(outage);
     };
 
     const outageHandler = (e) => {
@@ -27,16 +43,13 @@ const CheckComponent = (props) => {
     return (
         <div className={styles.Env}>
             <div className={styles.Check}>
-
                 <section>
-
                     <header className={styles.CheckHeader}>
                         <span>Name: {props.name}</span>
                         <span>Last shut: {props.lastShut ? moment(props.lastShut).format("MMMM Do YYYY, hh:mm") : 'unknown'}</span>
-                        <span>Last recovery: {props.lastRecovery? moment(props.lastRecovery).format("MMMM Do YYYY, hh:mm") : 'unknown'}</span>
+                        <span>Last recovery: {props.lastRecovery ? moment(props.lastRecovery).format("MMMM Do YYYY, hh:mm") : 'unknown'}</span>
                     </header>
                     <hr />
-
                     <p className={styles.CheckDetails}>
                         <span>Host: {props.host}</span>
                         <span>Type: {props.type}</span>
@@ -44,29 +57,25 @@ const CheckComponent = (props) => {
                     </p>
 
                 </section>
-
-
                 <section className={styles.OutageInfo}>
-                        <label>*Last N hours:</label>
-                        <form className={styles.OutageForm} onSubmit={submitHandler}>
-                            <input 
-                            id="interval" 
-                            type="number" 
-                            name="interval" 
-                            placeholder="168 hours default" 
-                            onChange={outageHandler} 
+                    <label>*Last N hours:</label>
+                    <form className={styles.OutageForm} onSubmit={submitHandler}>
+                        <input
+                            id="interval"
+                            type="number"
+                            name="interval"
+                            placeholder="168 hours default"
+                            onChange={outageHandler}
                             value={hours} />
-
-                                <input className={styles.Btn} type="submit" value="Outage"/>
-
-                        </form>
-                        <span className={styles.Outage}>Unknown<label>{outagePercentage.unknown}%</label></span>
-                        <span className={styles.Outage}>Up<label>{outagePercentage.up ? outagePercentage.up : 0}%</label></span>
-                        <span className={styles.Outage}>Down<label>{outagePercentage.down ? outagePercentage.down : 0}%</label></span>
+                        <input className={styles.Btn} type="submit" value="Outage" />
+                    </form>
+                    <span className={styles.Outage}>Unknown<label>{outage.unknown}%</label></span>
+                    <span className={styles.Outage}>Up<label>{outage.up}%</label></span>
+                    <span className={styles.Outage}>Down<label>{outage.down}%</label></span>
                 </section>
             </div>
         </div>);
 
 }
 
-            export default CheckComponent;
+export default CheckComponent;
